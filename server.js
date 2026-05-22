5ed0e3ff7517f7bfb1e9a44304812f8198c63770
JESM <joaquinezequielsm@gmail.com>
2026-05-22 16:33:22 -0300

    v2.2.3 polish: add custom favicon, remove floppy label, hide terminal cursor when blurred

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ app.get('*', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`\n=============================================================`);
-  console.log(`[SYSTEM] RetroMatch OS (v2.2.2 Universal Edition) Server Started.`);
+  console.log(`[SYSTEM] RetroMatch OS (v2.2.3 Universal Edition) Server Started.`);
   console.log(`[PORT]   Universal Port: http://localhost:${PORT}`);
   console.log(`=============================================================\n`);
 });
